c81449a58def8491290de22e50030da82844741f
feijinping <feijinping@foxmail.com>
2017-11-29 17:54:53 +0800

    ad

--- server/controllers/demo.js
+++ server/controllers/demo.js
@@ -1,4 +1,24 @@
+var mysql = require("mysql");
+const { mysql: config } = require('../config')
 module.exports=ctx=>{
+  const DB = require('knex')({
+    client: 'mysql',
+    connection: {
+      host: config.host,
+      port: config.port,
+      user: config.user,
+      password: config.pass,
+      database: config.db,
+      charset: config.char,
+      multipleStatements: true
+    }
+  })
+  DB.raw("select * from cSessionInfo").then(res => {
+    console.log('数据库初始化成功！')
+    process.exit(0)
+  }, err => {
+    throw new Error(err)
+  })
   ctx.state.data = {
     msg:"hello nihao"
   }
